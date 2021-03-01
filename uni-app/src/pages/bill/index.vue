15d4f9ae8ecdf0d345422ca151bbeed9348bc45f
Memoyu <mmy6076@outlook.com>
2021-03-01 18:06:46 +0800

    【Update】添加uni-app tabBar

--- uni-app/src/pages/bill/index.vue
+++ uni-app/src/pages/bill/index.vue
@@ -1,6 +1,5 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
 		<view>
 			<text class="title">{{title}}</text>
 		</view>
@@ -11,7 +10,7 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: '账单页面'
 			}
 		},
 		onLoad() {
---
+++ uni-app/src/pages/profile/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="content">
+		<view>
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '我的页面'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin: 200rpx auto 50rpx auto;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
