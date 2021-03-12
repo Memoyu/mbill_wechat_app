c643b0fb85096b355e1c501b053121482059198e
Memoyu <mmy6076@outlook.com>
2021-03-13 01:12:53 +0800

    【Update】 Start uni-app

--- wepy/src/store/reducers/data.js
+++
@@ -1,4 +0,0 @@
-export const billData = {
-  statementSubmiting: false,
-  statements: []
-}
--- wepy/src/store/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux'
-import bill from './bill'
-export default combineReducers({
-  bill
-})
