bf4287cabc45d49f481e8a2308d3d01a5fc94cab
Memoyu <mmy6076@outlook.com>
2021-03-16 02:06:43 +0800

    【Update】完成Echarts适配以及页面迁移
    仍旧采用wepy的方式进行适配，期间遇到些问题，待整理

--- src/src/components/mbill/mbill-chart/mbill-chart-overview.vue
+++ src/src/components/mbill/mbill-chart/mbill-chart-overview.vue
@@ -1,17 +1,159 @@
 <template>
-<text>总览</text>
+  <view class="overview">
+    <view class="overview-header">
+      <view class="overview-header__card">
+        <view class="expend amount">{{ header.expend }}</view>
+        <view class="text-small">总支出</view>
+      </view>
+      <view class="overview-header__card">
+        <view class="income amount">{{ header.income }}</view>
+        <view class="text-small">总收入</view>
+      </view>
+      <view class="overview-header__card">
+        <view class="transfer amount">{{ header.transfer }}</view>
+        <view class="text-small">转账</view>
+      </view>
+      <view class="overview-header__card">
+        <view class="expend amount">{{ header.repay }}</view>
+        <view class="text-small">还款</view>
+      </view>
+    </view>
+
+    <view class="overview__count-result">
+      总计（收入-支出-还款）：<text class="expend">{{ header.total }}</text>
+    </view>
+
+    <view class="overview__statements">
+      <view class="ovweview-title">账单列表</view>
+      <div v-for="(item, index) in statements" :key="index">
+        <mbill-bill-statement-item :bill="item"></mbill-bill-statement-item>
+      </div>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		name: "overviewChart",
-		props: {
-		},
-		methods: {
-		}
-	}
+export default {
+  name: "overviewChart",
+  props: {
+    date: String,
+  },
+  data() {
+    return {
+      header: {},
+      statements: [],
+      categories: [],
+    };
+  },
+  created() {
+    this.getHeader();
+    this.getStatementList();
+    // this.categoryChart()
+  },
+  watch: {
+    date() {
+      this.getHeader();
+      this.getStatementList();
+      // this.categoryChart()
+    },
+  },
+  methods: {
+    async getHeader() {
+      // const res = await wxRequest.Get('chart/overview_header', { date: this.date })
+      const res = {
+        expend: "20",
+        income: "20",
+        transfer: "20",
+        repay: "20",
+        total: "200",
+      };
+      this.header = res;
+    },
+    async getStatementList() {
+      //console.log("加载")
+      // const res = await wxRequest.Get('chart/overview_statements', { date: this.date })
+      const res = [
+        {
+          id: 1,
+          categoryIconPath: "",
+          categoryName: "数码",
+          description: "买电脑",
+          year: "2021",
+          month: "11",
+          day: "01",
+          time: "01:26",
+          assetName: "微信",
+          type: "expend",
+          amount: 300,
+        },
+        {
+          id: 2,
+          categoryIconPath: "",
+          categoryName: "出行",
+          description: "买电脑",
+          year: "2021",
+          month: "11",
+          day: "01",
+          time: "01:26",
+          assetName: "微信",
+          type: "expend",
+          amount: 300.89,
+        },
+      ];
+      this.statements = res;
+    },
+    async categoryChart() {
+      // const res = await wxRequest.Get('chart/overview_budgets', { date: this.date })
+      const res = [
+        { category_id: 1, percent: 30, name: "伙食", format_amount: 40 },
+        { category_id: 2, percent: 40, name: "游戏", format_amount: 40 },
+      ];
+      this.categories = res;
+      for (let item of res) {
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-		
+.overview {
+  .overview-header {
+    padding: 24rpx 0;
+    border-bottom: 1px solid #f4f4f4;
+    > .overview-header__card {
+      display: inline-block;
+      width: 25%;
+      text-align: center;
+      .text-small {
+        font-size: 24rpx;
+      }
+      .amount {
+        font-size: 32rpx;
+      }
+    }
+  }
+  .overview__count-result {
+    padding: 16rpx 24rpx;
+    font-size: 24rpx;
+  }
+  .overview__category-chart {
+    margin: 12px;
+    padding: 12px;
+    box-shadow: 0 0 4px #eee;
+    .budget-chart-item {
+      display: inline-block;
+      font-size: 24rpx;
+      text-align: center;
+    }
+  }
+  .overview__statements {
+    margin: 12px;
+    padding: 12px;
+    box-shadow: 0 0 4px #eee;
+  }
+  .ovweview-title {
+    font-size: 28rpx;
+  }
+}
 </style>
--- src/src/components/mbill/mbill-chart/mbill-chart-rate.vue
+++ src/src/components/mbill/mbill-chart/mbill-chart-rate.vue
@@ -1,17 +1,110 @@
 <template>
-<text>排行榜</text>
+  <view class="category">
+    <core-tabs :type="tabList" v-model="active"></core-tabs>
+
+    <div v-for="(item, index) in statements" :key="index">
+      <mbill-bill-statement-item :bill="item"></mbill-bill-statement-item>
+    </div>
+  </view>
 </template>
 
 <script>
-	export default {
-		name: "rateChart",
-		props: {
-		},
-		methods: {
-		}
-	}
+import mbillBillStatementItem from "../mbill-bill/mbill-bill-statement-item.vue";
+export default {
+  components: { mbillBillStatementItem },
+  name: "rateChart",
+  props: {},
+  data() {
+    return {
+      active: 0,
+      tabList: [
+        {
+          title: "支出",
+        },
+        {
+          title: "收入",
+        },
+      ],
+	  statements: [],
+    };
+  },
+  watch: {
+    date() {
+      this.getStatements();
+    },
+  },
+  created() {
+    this.getStatements();
+  },
+  methods: {
+    handleChange({ detail }) {
+      this.current = detail.key;
+      this.getStatements();
+    },
+    getStatements() {
+      // const res = await wxRequest.Get('chart/rate', { date: this.date, type: this.current })
+      this.statements = [
+         {
+          id: 1,
+          categoryIconPath: "",
+          categoryName: "数码",
+          description: "买电脑",
+          year: "2021",
+          month: "11",
+          day: "01",
+          time: "01:26",
+          assetName: "微信",
+          type: "expend",
+          amount: 300,
+        },
+        {
+          id: 2,
+          categoryIconPath: "",
+          categoryName: "出行",
+          description: "买电脑",
+          year: "2021",
+          month: "11",
+          day: "01",
+          time: "01:26",
+          assetName: "微信",
+          type: "expend",
+          amount: 300.89,
+        },
+      ];
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-		
+.expend-bars {
+  .item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #212121;
+    margin: 12px 0;
+    padding: 8px;
+    background-repeat: no-repeat;
+    background-image: linear-gradient(#c1f5e7, #c1f5e7);
+    border-radius: 4px;
+    .title {
+      font-size: 14px;
+    }
+    .amount {
+      margin-top: 4px;
+      font-size: 14px;
+      color: green;
+    }
+    .percent {
+      font-size: 12px;
+      margin-left: 12px;
+      color: #757575;
+    }
+  }
+  .zan-icon-arrow {
+    font-size: 14px;
+    color: #ccc;
+  }
+}
 </style>
