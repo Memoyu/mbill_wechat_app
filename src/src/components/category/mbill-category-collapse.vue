75f1d2dfd40bf7fbef25c6406ff1d6cd47753a7e
Memoyu <mmy6076@outlook.com>
2022-07-22 23:52:55 +0800

    fix：adjust category manage page

--- src/src/components/category/mbill-category-collapse.vue
+++ src/src/components/category/mbill-category-collapse.vue
@@ -1,6 +1,10 @@
 <template>
-  <view class="bill-asset-manage">
-    <view class="asset-collapse-content">
+  <view class="mbill-category-collapse">
+    <scroll-view
+      scroll-y="true"
+      class="mbill-category-collapse-scroll-view"
+      :style="{ height: height - 15 + 'px' }"
+    >
       <uni-collapse>
         <uni-collapse-item
           v-for="(g, index) in groups"
@@ -10,9 +14,9 @@
           :open="index === 0"
         >
           <template v-slot:title>
-            <view class="asset-group">{{ g.name }}</view>
+            <view class="category-group">{{ g.name }}</view>
           </template>
-          <view class="asset-item">
+          <view class="category-item">
             <view
               :class="['item-content']"
               v-for="(item, index) in g.childs"
@@ -24,69 +28,50 @@
             </view>
             <view
               class="item-content item-add-content"
-              @click="handleAddAsset(g.id)"
+              @click="handleAddCategory(g.id)"
             >
               <image class="image" src="/static/assets/add.png" />
             </view>
           </view>
         </uni-collapse-item>
       </uni-collapse>
-    </view>
-    <view class="add-asset-group" @tap="handleAddGroup">添加分组</view>
+    </scroll-view>
   </view>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      groups: [],
-    };
-  },
-  onLoad() {
-    this.getAssetGroups();
-  },
-  onShow() {},
-  methods: {
-    // 选中账户
-    handleSelectedItem(asset) {},
-
-    // 添加分类
-    handleAddAsset(groupId) {
-      console.log(groupId, "分组Id");
-      this.$tip.toast("开发中，敬请期待");
+  name: "mbill-category-collapse",
+  props: {
+    height: {
+      type: [Number, String],
+      default: 300,
     },
-
-    // 添加分组
-    handleAddGroup() {
-      this.$tip.toast("开发中，敬请期待");
-    },
-
-    // 获取账单账户
-    getAssetGroups() {
-      this.$api.assetGroups().then((res) => {
-        if (res.data.code === 0) {
-          this.groups = res.data.result;
-        }
-      });
+    groups: {
+      type: Array,
+      default: () => [],
     },
   },
+  methods: {},
 };
 </script>
 
 <style lang="scss" scope>
-.bill-asset-manage {
-  padding-top: 102rpx;
-  .asset-collapse-content {
-    background: #ffffff;
-    border-radius: 30rpx;
-    margin: 0 10rpx;
-    padding: 10rpx;
-    .asset-group {
+.mbill-category-collapse {
+  background: #ffffff;
+  border-radius: 30rpx;
+  display: flex;
+  margin: 0 10rpx;
+  padding: 10rpx;
+  &-scroll-view {
+    ::-webkit-scrollbar {
+      display: none;
+    }
+    .category-group {
       font-weight: bold;
       padding: 20rpx 30rpx;
     }
-    .asset-item {
+    .category-item {
       display: grid;
       grid-template-columns: repeat(auto-fill, 140rpx);
       justify-content: center;
@@ -113,13 +98,9 @@ export default {
         align-items: center;
       }
     }
-  }
-
-  .add-asset-group {
-    color: $dark-color;
-    font-weight: bold;
-    margin: 40rpx 0;
-    text-align: center;
+    .category-item-selected {
+      background-color: $light-color;
+    }
   }
 }
 </style>
--- src/src/pages/bill/category/manage.vue
+++ src/src/pages/bill/category/manage.vue
@@ -1,45 +1,48 @@
 <template>
   <view class="bill-category-manage">
-    <view class="type-tabs">
+    <view class="type-tabs" id="header-type-tabs">
       <mb-ba-focus-tabs
         v-model="type"
         :items="types"
         @selected="handleTypeSwitch"
       />
     </view>
-    <view class="category-collapse-content">
-      <uni-collapse>
-        <uni-collapse-item
-          v-for="(g, index) in groups"
-          :key="index"
-          :border="false"
-          title-border="none"
-          :open="index === 0"
-        >
-          <template v-slot:title>
-            <view class="category-group">{{ g.name }}</view>
-          </template>
-          <view class="category-item">
-            <view
-              :class="['item-content']"
-              v-for="(item, index) in g.childs"
-              :key="index"
-              @click="handleSelectedItem(item)"
-            >
-              <image class="image" :src="item.iconUrl" />
-              <text class="text">{{ item.name }}</text>
-            </view>
-            <view
-              class="item-content item-add-content"
-              @click="handleAddCategory(g.id)"
-            >
-              <image class="image" src="/static/assets/add.png" />
-            </view>
-          </view>
-        </uni-collapse-item>
-      </uni-collapse>
-    </view>
-    <view class="add-category-group" @tap="handleAddGroup">添加分组</view>
+    <swiper
+      :current="type"
+      @change="tabChange"
+      :style="{ height: contentH + 'px' }"
+    >
+      <swiper-item class="swiper_item">
+        <mb-ca-collapse :height="contentH" :groups="groups" />
+      </swiper-item>
+      <swiper-item class="swiper_item">
+        <mb-ca-collapse :height="contentH" :groups="groups1" />
+      </swiper-item>
+    </swiper>
+
+    <view class="add-category-group" id="bottom-add-group" @tap="handleAddGroup"
+      >添加分组</view
+    >
+
+    <!-- 添加分组弹窗 -->
+    <mb-ba-edit-dialog
+      ref="addGroupDialog"
+      height="170"
+      class="edit-group-input"
+      @change="handleDialogChange"
+      @ltap="handleReqEditGroup"
+    >
+      <view class="input-item x-bc">
+        <text class="title">分组名称</text>
+        <input
+          type="text"
+          class="input"
+          v-model="group.name"
+          alwaysEmbed="true"
+          placeholder="名称"
+        />
+      </view>
+    </mb-ba-edit-dialog>
   </view>
 </template>
 
@@ -53,9 +56,18 @@ export default {
         { key: 1, text: "收入" },
       ],
       groups: [],
+      groups1: [],
+      contentH: 0,
     };
   },
+  watch: {
+    type(val) {
+      console.log("账单类型", val);
+      this.getCategoryGroups(val);
+    },
+  },
   onLoad(option) {
+    this.dynamicHeight();
     if (option.type != undefined) {
       this.type = option.type;
     }
@@ -63,6 +75,11 @@ export default {
   },
   onShow() {},
   methods: {
+    tabChange({ detail }) {
+      let curr = detail.current;
+      this.type = curr;
+    },
+
     // 选中分类
     handleSelectedItem(category) {},
 
@@ -79,8 +96,8 @@ export default {
 
     // 切换分类类型
     handleTypeSwitch(type) {
-      // this.type = key;
-      this.getCategoryGroups(type);
+      this.type = type;
+      // this.getCategoryGroups(type);
     },
 
     // 获取账单分类
@@ -88,9 +105,31 @@ export default {
       this.$api.categoryGroups({ type: type }).then((res) => {
         if (res.data.code === 0) {
           // this.groups = [];
-          this.groups = res.data.result;
-          this.$forceUpdate();
+          if (type == 0) {
+            this.groups = res.data.result;
+          } else {
+            this.groups1 = res.data.result;
+          }
         }
+      });
+    },
+
+    // 计算高度，动态调整swiper 高度
+    dynamicHeight() {
+      let that = this;
+      uni.getSystemInfo({
+        success(res) {
+          let pH = res.windowHeight;
+          let query = uni.createSelectorQuery().in(that);
+          query.select("#header-type-tabs").fields({ size: true });
+          query.select("#bottom-add-group").fields({ size: true });
+          query.exec((data) => {
+            let headerH = data[0].height;
+            headerH += data[1].height;
+            that.contentH = pH - headerH - 40; // 40 为margin
+            // console.log(pH, headerH, that.contentH);
+          });
+        },
       });
     },
   },
@@ -106,51 +145,11 @@ export default {
     justify-content: center;
     align-items: center;
   }
-  .category-collapse-content {
-    background: #ffffff;
-    border-radius: 30rpx;
-    margin: 0 10rpx;
-    padding: 10rpx;
-    .category-group {
-      font-weight: bold;
-      padding: 20rpx 30rpx;
-    }
-    .category-item {
-      display: grid;
-      grid-template-columns: repeat(auto-fill, 140rpx);
-      justify-content: center;
-      .item-content {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        border-radius: 20rpx;
-        padding: 14rpx 0;
-        .image {
-          height: 70rpx;
-          width: 70rpx;
-        }
-        .text {
-          margin-top: 10rpx;
-          color: $grey-black-text-color;
-          font-size: 28rpx;
-        }
-      }
-      .item-add-content {
-        display: flex;
-        justify-content: center;
-        flex-direction: unset;
-        align-items: center;
-      }
-    }
-    .category-item-selected {
-      background-color: $light-color;
-    }
-  }
 
   .add-category-group {
     color: $dark-color;
     font-weight: bold;
-    margin-top: 40rpx;
+    margin: 40rpx 0;
     text-align: center;
   }
 }
